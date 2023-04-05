6b26f4d3c140781f5c9f56e317c6ac466e7fa01b
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-04-05 10:41:39 +0200

    Preliminary tryBuildComment to Lexer added

--- src/main/java/tkom/lexer/Lexer.java
+++ src/main/java/tkom/lexer/Lexer.java
@@ -22,7 +22,7 @@ public class Lexer {
     public char currChar;
     Position currPos;
 
-    int MAX_LENGTH = 200; // maximum number of chars available in a string or a comment
+    static final int MAX_LENGTH = 200; // maximum number of chars available in a string or a comment
 
     public Lexer(BufferedReader br1) throws IOException {
 
@@ -35,13 +35,18 @@ public class Lexer {
         nextChar();
     }
 
+    /**
+     * Assigns next character from the input stream to current character,
+     * but omits any whitespace characters.
+     * On EOF character finishes and makes the Lexer stop the work.
+     * @throws IOException
+     */
     public void nextChar() throws IOException {
         int currInt=br.read();
         currPos.rowNo++;
         while (currInt != -1 && Character.isWhitespace((char)currInt)){
             if ((char)currInt == '\n')
                 currPos.colNo++;
-            System.out.println((char)currInt);
             currInt=br.read();
         }
         if (currInt == -1) {
@@ -51,6 +56,25 @@ public class Lexer {
         }
         currChar = (char)currInt;
     }
+
+    public void nextCharCommText() throws IOException {
+        int currInt=br.read();
+        currPos.rowNo++;
+        if (currInt == -1) {
+            running = false;
+            // End of file
+            //TODO: smarter return
+        }
+        if ((char)currInt == '\n')
+            currPos.colNo++;
+        currChar = (char)currInt;
+    }
+
+    /**
+     * Tries building one of the single or double signs from input characters
+     * Example tokens are: '<' T_LESS or '==' T_EQUALS
+     * @throws IOException
+     */
 
     public void tryBuildSign() throws IOException {
         Position startToken = currPos;
@@ -73,7 +97,12 @@ public class Lexer {
         newToken = t;
     }
 
-    public void tryBuildString() throws IOException {
+    /**
+     * Tries building an identifier: either user's custom defined variable
+     * or a language keyword e.g. 'if'
+     * @throws IOException
+     */
+    public void tryBuildIdent() throws IOException {
         Position startToken = currPos;
         Token t;
         StringBuilder builder = new StringBuilder();
@@ -92,6 +121,10 @@ public class Lexer {
         newToken = t;
     }
 
+    /**
+     * Tries building a regular number: both integer and double values are supported.
+     * @throws IOException
+     */
     public void tryBuildNumber() throws IOException {
         Position startToken = currPos;
         int value = Character.getNumericValue(currChar);
@@ -109,21 +142,78 @@ public class Lexer {
         Token t = new Token(TokenType.T_INT, Integer.toString(value), startToken);
         newToken = t;
     }
-    public void tryBuildNumOrString() throws IOException {
+
+    /**
+     * Depending on first character tries building either a number or an identifier
+     * @throws IOException
+     */
+    public void tryBuildNumOrIdent() throws IOException {
         if (Character.isLetter(currChar))
-            tryBuildString();
+            tryBuildIdent();
         else if (Character.isDigit(currChar))
             tryBuildNumber();
         else
             System.out.println(currChar);
     }
 
+    /**
+     * Tries building a comment that starts with '#' character and ends after a newline character.
+     * Maximum possible length of comment is MAX_LENGTH.
+     * Next input characters are obtained differently as newline character should not be omitted
+     * as it ends the comment.
+     * @throws IOException
+     */
+    public void tryBuildComment() throws IOException {
+        int commentLen = 0;
+        Position startToken = currPos;
+        StringBuilder builder = new StringBuilder();
+        while (currChar != '\n' || commentLen>MAX_LENGTH){
+            commentLen++;
+            builder.append(currChar);
+            nextCharCommText();
+        }
+        if (currChar == '\n')
+            nextChar();
+        if (commentLen > MAX_LENGTH){
+            //TODO: throw an exception
+            System.out.println("Too long comment");
+        }
+        String comment = builder.toString();
+        Token t = new Token(TokenType.T_COMMENT, comment, startToken);
+    }
+
+    /**
+     * Tries building a text string that starts and ends with a ' " ' character.
+     * Maximum possible length of text is MAX_LENGTH.
+     * Next input characters are obtained differently as escape characters need to be handled
+     * and characters as newline or tab should be included in the resulting string.
+     * @throws IOException
+     */
+//    public void tryBuildText() throws IOException {
+//        int textLen = 0;
+//        Position startToken = currPos;
+//        StringBuilder builder = new StringBuilder();
+//        while (currChar != '\"' || textLen>MAX_LENGTH){
+//        }
+//    }
+
+    /**
+     * Depending on first character invokes functions that will start building
+     * appropriate tokens.
+     * @throws IOException
+     */
     public void getToken() throws IOException {
         if (Character.isLetterOrDigit(currChar))
-            tryBuildNumOrString();
+            tryBuildNumOrIdent();
         else if (TMap.T_SIGNS.containsKey(Character. toString(currChar))){
             tryBuildSign();
         }
+        else if (currChar=='#') {
+            tryBuildComment();
+        }
+//        else if (currChar=='\"') {
+//            tryBuildText();
+//        }
         else {
             //TODO: throw Exception
             System.out.println("Unknown sign");
